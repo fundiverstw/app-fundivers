ba4c5adc178b38e63bc49ce71a04e4cd53ebabbf
ericodle <504068014080@tutanota.com>
2026-05-08 21:00:00 +0800

    update wix pages

--- wix/upcoming_dives/upcoming_dives.js
+++ wix/upcoming_dives/upcoming_dives.js
@@ -26,7 +26,7 @@ $w.onReady(async () => {
 
   const payload = {
     id: dive._id,
-    title: dive.dive_title || dive.title || 'Untitled dive',
+    title: dive.display_title || dive.admin_title || 'Untitled dive',
     image: dive.featured_image || '',
     schedule: buildSchedule(dive.start_date, dive.end_date, dive.time),
     price: dive.starting_at,
